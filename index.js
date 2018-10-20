a4e0f0492e44a8516f97c6df7213d9e1612cfd0e
Mohammed Chisti <93mchisti@gmail.com>
2018-10-20 11:23:53 -0400

    Added some item routes

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
+const items = require('./routes/items')
 const express = require('express')
 const app = express()
 
 app.use(express.json())
+app.use('/api/items', items)
 
 app.get('/', (req, res) => {
-  res.send([{ message: "Hello!" }])
+  res.send([{ message: "Hello! Welcome to the Price List API!" }])
 })
 
 var port = process.env.PORT || 5000
---
+++ routes/items.js
@@ -0,0 +1,55 @@
+const express = require('express')
+const router = express.Router()
+
+// Temp Data
+var items = [
+  {
+    id: 1,
+    name: "Apple",
+    price: 0.59,
+    unitPrice: 0.59,
+    unit: "apple",
+    location: "Ideals, 100-48",
+    date: "18102018",
+    active: true
+  },
+  {
+    id: 2,
+    name: "Beer",
+    price: 13.75,
+    unitPrice: 2.20,
+    unit: "beer",
+    location: "Ideals, 100-48",
+    date: "18102018",
+    active: false
+  },
+]
+
+router.get("/", (req, res) => {
+  res.send(items)
+})
+
+router.get("/:id", (req, res) => {
+  const item = items.find(item => parseInt(req.params.id) === item.id)
+
+  res.send(item)
+})
+
+router.post("/", (req, res) => {
+  const item = {
+    id: items.length + 1,
+    name: req.body.name,
+    price: req.body.price,
+    unitPrice: req.body.unitPrice,
+    unit: req.body.unit,
+    location: req.body.location,
+    date: req.body.date,
+    active: true
+  }
+
+  items.push(item)
+
+  res.send(item)
+})
+
+module.exports = router
